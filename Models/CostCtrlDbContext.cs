8a7638adedb50012e4f99fbfc70c3cf359eea30a
Angelo Panariti <angpanariti@gmail.com>
2025-07-07 14:36:16 +0200

    Add project files.

---
+++ Models/CostCtrlDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CostCtrl.Models
+{
+    public class CostCtrlDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public CostCtrlDbContext(DbContextOptions<CostCtrlDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+    }
+}
